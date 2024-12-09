73d98b0746ecc9f211c137f24afedd6e48340bfc
Arianah255 <160747680+Arianah255@users.noreply.github.com>
2024-12-08 20:13:46 -0500

    Update DetallePedido.cs

--- Models/DetallePedido.cs
+++ Models/DetallePedido.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace BoxNovaDB.Models
+namespace BoxNovaSoftAPI.Models
 {
     public class DetallePedido
     {
