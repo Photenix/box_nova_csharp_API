9b7c64419a34fdfdf30d444d6a39c0f6ff80fe44
Arianah255 <160747680+Arianah255@users.noreply.github.com>
2024-12-08 20:12:56 -0500

    Update Venta.cs

--- Models/Venta.cs
+++ Models/Venta.cs
@@ -3,7 +3,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Collections.Generic;
 
-namespace BoxNovaDB.Models
+namespace BoxNovaSoftAPI.Models
 {
     public class Venta
     {
