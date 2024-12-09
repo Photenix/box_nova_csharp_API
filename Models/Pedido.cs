eec5d9952d65ffa515b5c1a2556f5ee85125c3ca
Arianah255 <160747680+Arianah255@users.noreply.github.com>
2024-12-08 20:11:06 -0500

    Update Pedido.cs

--- Models/Pedido.cs
+++ Models/Pedido.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace WebEmpresa.Models
+namespace BoxNovaDB.Models
 {
     public class Pedido
     {
