8234bb247c5aa69635da766ed32b4cbb37cb3daf
Sebasber1211 <10a21bertelmarzolajuan@gmail.com>
2024-12-10 07:00:43 -0500

    context

--- Models/BoxNovaDbContext.cs
+++ Models/BoxNovaDbContext.cs
@@ -22,6 +22,12 @@ public partial class BoxNovaDbContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
+    public virtual DbSet<Cliente> Clientes { get; set; } = null!;
+    public virtual DbSet<Carrito> Carritos { get; set; } = null!;
+    public virtual DbSet<CategoriaProducto> CategoriaProductos { get; set; } = null!;
+    public virtual DbSet<SubCategoriaProducto> SubCategoriaProductos { get; set; } = null!;
+
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<PerXrolXpriv>(entity =>
@@ -148,6 +154,57 @@ public partial class BoxNovaDbContext : DbContext
                 .HasForeignKey(d => d.FkRol)
                 .HasConstraintName("fk_rol_user");
                 //.OnDelete(DeleteBehavior.Restrict)//.OnDelete(DeleteBehavior.ClientSetNull)
+        });
+
+        // Configuración de Cliente
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.IdCliente);
+            entity.Property(e => e.NombreCliente).IsRequired().HasMaxLength(30);
+            entity.Property(e => e.ApellidoCliente).IsRequired().HasMaxLength(30);
+            entity.Property(e => e.CedulaCliente).IsRequired().HasMaxLength(20);
+            entity.HasIndex(e => e.CedulaCliente).IsUnique();
+            entity.Property(e => e.GeneroCliente).HasMaxLength(1);
+            entity.Property(e => e.FechaRegistro).IsRequired();
+
+            entity.HasMany(e => e.Carritos)
+                  .WithOne(e => e.Cliente)
+                  .HasForeignKey(e => e.IdCliente)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // Configuración de Carrito
+        modelBuilder.Entity<Carrito>(entity =>
+        {
+            entity.HasKey(e => e.IdDetalle);
+            entity.Property(e => e.Cantidad).IsRequired().HasDefaultValue(1);
+            entity.Property(e => e.PrecioUnitario).IsRequired();
+            entity.Property(e => e.Subtotal).HasComputedColumnSql("[Cantidad] * [PrecioUnitario]");
+
+            entity.HasOne(e => e.Cliente)
+                  .WithMany(e => e.Carritos)
+                  .HasForeignKey(e => e.IdCliente);
+        });
+
+        // Configuración de CategoriaProducto
+        modelBuilder.Entity<CategoriaProducto>(entity =>
+        {
+            entity.HasKey(e => e.IdCProd);
+            entity.Property(e => e.NombreCProd).IsRequired().HasMaxLength(30);
+            entity.Property(e => e.EstadoCProd).IsRequired();
+        });
+
+        // Configuración de SubCategoriaProducto
+        modelBuilder.Entity<SubCategoriaProducto>(entity =>
+        {
+            entity.HasKey(e => e.IdSubCProd);
+            entity.Property(e => e.NombreCProd).IsRequired().HasMaxLength(30);
+            entity.Property(e => e.EstadoCProd).IsRequired();
+
+            entity.HasOne(e => e.Categoria)
+                  .WithMany(e => e.SubCategorias)
+                  .HasForeignKey(e => e.IdCProd)
+                  .OnDelete(DeleteBehavior.Cascade);
         });
 
         OnModelCreatingPartial(modelBuilder);
